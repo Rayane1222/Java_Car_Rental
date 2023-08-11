450d09a21a3ba424d9b0dbb2c298c8c52c57d867
mac <mac@localhost>
2023-08-11 17:39:53 +0100

    first Commit

--- src/java_car_rental/Form_Login.java
+++ src/java_car_rental/Form_Login.java
@@ -16,7 +16,7 @@ public class Form_Login extends javax.swing.JFrame {
     public Form_Login() {
         initComponents();
     }
-
+//
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
